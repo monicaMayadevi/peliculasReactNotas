d479f9affa6b06a919671b908904cce6c2b5188c
Monica Silva <msilva@correo.ler.uam.mx>
2021-10-28 16:30:41 -0500

    agregar listado a store peliculas

--- src/stores/PeliculasStore.js
+++ src/stores/PeliculasStore.js
@@ -5,9 +5,10 @@ export default class PeliculasStore
 	{ makeAutoObservable(this) }
   titulo = "Video centro"
    listado = [
-		{ nombre: 'Viernes 13', genero: 'Terror', clasificacion: 'C' },
-		{ nombre: 'Bambi', genero: 'Infantil', clasificacion: 'A' },
-		{ nombre: 'XXX', genero: 'Accion', clasificacion: 'B' },
+		{ nombre: 'Viernes 13', genero: 'Terror', clasificacion: 'B' },
+ 		{ nombre: 'Bambi', genero: 'Infantil', clasificacion: 'A' },
+    { nombre: 'Rambo', genero: 'Accion', clasificacion: 'B' },
+    { nombre: 'XXX', genero: 'Accion', clasificacion: 'C' }
 		]
 
 
